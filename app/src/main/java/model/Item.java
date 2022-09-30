89d53d37d61736c10649b2ec80d1e4275cdc2198
Caroline Akesson <ca223pw@student.lnu.se>
2022-09-30 16:45:54 -0500

    Create Item class basic structure

--- app/src/main/java/model/Item.java
+++ app/src/main/java/model/Item.java
@@ -4,12 +4,48 @@ package model;
  * Class representing an Item.
  */
 public class Item {
-  /**
-   * Returns a very important message.
+  private String category;
+  private String name;
+  private String description;
+  private int costPerDay;
+  private int dayCreated;
 
-   * @return the message.
-   */
-  public String getMessage() {
-    return "Hello Item Model";
+  public Item(String category, String name, String description, int costPerDay, int dayCreated) {
+    setCategory(category);
+    setName(name);
+    setDescription(description);
+    setCostPerDay(costPerDay);
+    setDayCreated(dayCreated);
+  }
+
+  public String getCategory() {
+    return category;
+  }
+  public void setCategory(String category) {
+    this.category = category;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getDescription() {
+    return description;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  public int getCostPerDay() {
+    return costPerDay;
+  }
+  public void setCostPerDay(int costPerDay) {
+    this.costPerDay = costPerDay;
+  }
+  public int getDayCreated() {
+    return dayCreated;
+  }
+  public void setDayCreated(int dayCreated) {
+    this.dayCreated = dayCreated;
   }
 }
