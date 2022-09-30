c805b1e6c3d77802d3c6b8a32f3e10137cd4cd06
Caroline Akesson <ca223pw@student.lnu.se>
2022-09-29 20:15:03 -0500

    Add first draft menus

--- app/src/main/java/controller/App.java
+++ app/src/main/java/controller/App.java
@@ -6,15 +6,40 @@ package controller;
 public class App {
   /**
    * Application starting point.
-
+   *
    * @param args command line arguments.
    */
   public static void main(String[] args) {
     // Adapt to start the application in your way
-    model.Item item = new model.Item();
-    Member member = new Member("Caroline", "c@c.com", "0702018918", "r1a2n3d4o5m");
+    // model.Item item = new model.Item();
+    // controller.StuffLendingSystem stuffLendingSystem = new
+    // controller.StuffLendingSystem();
     view.UserInterface ui = new view.UserInterface();
+    int optionMainMenu = 0;
+    int optionMemberMenu = 0;
+    int optionItemMenu = 0;
 
-    member.doSomethingSimple(item, ui);
+    do {
+      optionMainMenu = ui.mainMenu();
+
+      switch (optionMainMenu) {
+        case 1:
+          do {
+            optionMemberMenu = ui.memberMenu();
+          } while (optionMemberMenu != 7);
+          break;
+        case 2:
+          do {
+            optionItemMenu = ui.itemMenu();
+          } while (optionItemMenu != 5);
+          break;
+        case 3:
+          System.out.println("Exiting...");
+          break;
+        default:
+          System.out.println("Option is invalid");
+          break;
+      }
+    } while (optionMainMenu != 3);
   }
 }
